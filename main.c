347fab5be457007922b33dc7fd6e6c17e8637310
Tanish Ohri Verse🧑‍💻 <tanishohri1935@gmail.com>
2025-04-06 12:25:33 +0530

    hi

--- main.c
+++ main.c
@@ -16,12 +16,13 @@ typedef struct {
 
 //property = pro
 void displaypro(property details[],int count);
-//Search by nd
+//Search by neighborhood
 void bynd(property details[],int n,char nh[]);
 //Filter by price
 void byprice(property details[],int price);
 
 int main() {
+    // All Data
     property details[area]={
         {101,7500000,"Park","Verified by Govt. Registry","OTP-based login"},
         {102,12000000,"Coastal Area, Hospitals, Malls","No hidden charges","Biometric login"},
@@ -45,34 +46,60 @@ int main() {
         {120,14000000,"Riverside, Schools, Local Shops","Govt. certified rates","Password-protected login"},
     };
 
-    /*int count = 20;
-    printf("Data Property\n");
-    displaypro(details,count);
-    return 0;*/
+    int choice;
 
-    int count = 20;
-    printf("Enter the you looking for:");
-    scanf("%s",nh);
-    byprice(details,count,char nh);
+        printf("What would you like to do?\n");
+        printf("1. Display all Properties\n");
+        printf("2. Search Property By Neighborhood\n");
+        printf("3. Search Property By Price\n");
+        printf("4. Exit\n");
 
+        printf("Enter you choice(1-3): ");
+        scanf("%d",&choice);
+
+        //if Statement
+        switch(choice) {
+            case 1:
+                int count = 20;
+                printf("Data Property\n");
+                displaypro(details,count);
+                break;
+            case 2:
+                int y = 20;
+                char nh[100];
+                printf("Enter the neighborhood you are looking for:");
+                scanf("%s",nh);
+                bynd(details,y,nh);
+                break;
+            /*case 3:
+                byprice();
+                break;*/
+            default:
+                printf("Wrong choice\n");
+                break;
+        }
+
+return 0;
 }
 
+//Funtion to display all properties
 void displaypro(property details[],int count) {
-    printf("Property Details\n");
+    printf("Property Details\n\n\n");
     for(int i=0;i<count;i++) {
-        printf("Property ID:");
-        printf("%d\t ",details[i].proID);
-        printf("Price:");
-        printf("%d\t",details[i].price);
-        printf("Neighborhood:");
-        printf("%s\t",details[i].nh);
-        printf("Pricing Detials:");
-        printf("%s\t",details[i].tpd);
-        printf("Authentication Type:");
-        printf("%s\t\n",details[i].at);
+        //printf("Property ID:");
+        printf("ID: %d\n",details[i].proID);
+        //printf("Price:");
+        printf("Price: %d\n",details[i].price);
+        //printf("Neighborhood:");
+        printf("NH: %s\n",details[i].nh);
+        //printf("Pricing Detials:");
+        printf("TPD: %s\n",details[i].tpd);
+        //printf("Authentication Type:");
+        printf("AT: %s\n\n\n\n",details[i].at);
     }
 }
 
+// Funtion to Search by neighborhood
 void bynd(property details[], int n, char nh[]) {
     int found = 0;
     printf("Searching for properties near: %s\n", nh);
